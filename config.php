4133eccde3226a2a225b1e467f51dfee444dd0ef
middlefinger66 <82159728+middlefinger66@users.noreply.github.com>
2021-04-08 20:34:29 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,5 @@
 $inputList = ''; // list.txt
 
 // telegram
-$telegramToken = ''; // token telegram bot
-$telegramChatId = ''; // chat id telegram
+$telegramToken = '1796066165'; // token telegram bot
+$telegramChatId = '1515834132'; // chat id telegram
